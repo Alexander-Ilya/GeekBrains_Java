ad675e369811c4cf045434f54e8d3a93ccf650ff
Alex <252459@gmail.com>
2022-04-19 09:45:29 +0300

    Solution of the second task

---
+++ src/main/java/ru/gb/ilyashuk/homework2/Task1.java
@@ -0,0 +1,38 @@
+package ru.gb.ilyashuk.homework2;
+
+public class Task1 {
+    //1
+    public static boolean sumWithIn(int a, int b) {
+        return (a + b >= 10 && a + b <= 20);
+    }
+
+    //2
+    public static void positiveNegative(int a) {
+        System.out.println(a >= 0 ? "Positive" : "Negative");
+    }
+
+    //3
+    public static boolean positiveOrNegative(int a) {
+        return (a < 0);
+    }
+
+    //4
+    public static void printingMultipleTimes(String word, int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println(word);
+        }
+    }
+
+    //5*
+    public static boolean isLeapYear(int year) {
+        return ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumWithIn(10, 1));
+        positiveNegative(-5);
+        System.out.println(positiveOrNegative(3));
+        printingMultipleTimes("Hello Task 2", 10);
+        System.out.println(isLeapYear(200));
+    }
+}
