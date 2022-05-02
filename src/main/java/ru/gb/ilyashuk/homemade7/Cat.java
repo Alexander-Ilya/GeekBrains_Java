7891fec5f5c743ed3c34e848ade8f14bb606f79e
Alex <252459@gmail.com>
2022-05-02 17:43:49 +0300

    Solution of the task7

---
+++ src/main/java/ru/gb/ilyashuk/homemade7/Cat.java
@@ -0,0 +1,23 @@
+package ru.gb.ilyashuk.homemade7;
+
+public class Cat {
+    private String name;
+    private int appetite;
+    private boolean isSatiety = false;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public void info() {
+        System.out.println(name + " " +(isSatiety ? "не голоден": "голоден и хочет съесть " + appetite + " кусочков еды."));
+    }
+
+    public void eat(Plate p) {
+        if (!isSatiety) {
+            isSatiety = p.decreaseFood(appetite);
+        }
+    }
+}
+
---
+++ src/main/java/ru/gb/ilyashuk/homemade7/Task7.java
@@ -0,0 +1,23 @@
+package ru.gb.ilyashuk.homemade7;
+
+public class Task7 {
+    public static void main(String[] args) {
+        Cat[] catArray = new Cat[(int) (1 + Math.random() * 9)];
+        for (int i = 0; i < catArray.length; i++) {
+            catArray[i] = new Cat("cat " + (i + 1), (int) (1 + Math.random() * 29));
+        }
+
+        Plate plate = new Plate(50);
+        plate.addFood(50);
+        plate.info();
+        for (Cat cat : catArray) {
+
+            cat.info();
+            cat.eat(plate);
+            cat.info();
+            plate.info();
+        }
+
+
+    }
+}
